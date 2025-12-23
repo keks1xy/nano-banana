d5ff921eca0a207934ccdb813781985fd4e7810f
keks1xy <56412618+keks1xy@users.noreply.github.com>
2025-12-23 12:12:18 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,31 @@
-from flask import Flask
+from flask import Flask, request, render_template_string
 
 app = Flask(__name__)
 
-@app.route("/")
+HTML = """
+<!doctype html>
+<html>
+<head>
+  <title>Nano Banana</title>
+</head>
+<body>
+  <h1>Nano Banana 🍌</h1>
+
+  <form method="post">
+    <input name="prompt" placeholder="Введите текст" style="width:300px">
+    <button type="submit">Отправить</button>
+  </form>
+
+  {% if prompt %}
+    <p><b>Вы ввели:</b> {{ prompt }}</p>
+  {% endif %}
+</body>
+</html>
+"""
+
+@app.route("/", methods=["GET", "POST"])
 def home():
-    return "It works 🚀"
+    prompt = None
+    if request.method == "POST":
+        prompt = request.form.get("prompt")
+    return render_template_string(HTML, prompt=prompt)
