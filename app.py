3c131cb8a873fa75605e281749f5249619922e64
keks1xy <56412618+keks1xy@users.noreply.github.com>
2025-12-23 12:17:30 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 from flask import Flask, request, render_template
+import time
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def home():
     prompt = None
+    status = None
+
     if request.method == "POST":
         prompt = request.form.get("prompt")
-    return render_template("index.html", prompt=prompt)
+        status = "processing"
+        time.sleep(2)  # имитация работы
+        status = "done"
+
+    return render_template("index.html", prompt=prompt, status=status)
